1b66fd10fa50a814379a67da0b17ab07dcf36419
Chaiene Krebs <chay.krebs@gmail.com>
2023-05-13 20:51:37 -0300

    Removidas libs inutilizadas

---
+++ Contatos.Helpers/Texto.cs
@@ -0,0 +1,19 @@
+﻿namespace Contatos.Helpers
+{
+    public static class Texto
+    {
+        public static string RemoverAcentos(this string texto)
+        {
+            if (String.IsNullOrEmpty(texto)) return string.Empty;
+
+            const string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+            const string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+            for (var i = 0; i < comAcentos.Length; i++)
+                texto = texto.Replace(comAcentos[i].ToString(), semAcentos[i].ToString());
+
+            return texto;
+        }
+
+    }
+}
